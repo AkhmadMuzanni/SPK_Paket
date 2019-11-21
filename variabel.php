f30c2bf37920048d54c7e6ba5692866247be23ae
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2019-11-22 00:35:34 +0700

    Jquery

--- variabel.php
+++ variabel.php
@@ -416,7 +416,7 @@ if ($result = mysqli_query($link, $sql)) {
 <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
 <!-- JavaScript Libraries -->
-<script src="lib/jquery/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="lib/bootstrap/js/bootstrap.min.js"></script>
 <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 <script src="lib/venobox/venobox.min.js"></script>
