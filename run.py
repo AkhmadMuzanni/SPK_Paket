ff5ef0509907f5fe124a3968a780a1ea98282237
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2019-11-22 01:15:24 +0700

    not http

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plot
 import mysql.connector as ms
 
 def getKategori():    
-    conn = ms.connect(user='root', password='bevy2019', host='http://ec2-34-207-250-194.compute-1.amazonaws.com', database='fuzzymamdani')
+    conn = ms.connect(user='root', password='bevy2019', host='ec2-34-207-250-194.compute-1.amazonaws.com', database='fuzzymamdani')
     # conn = ms.connect(user='root', password='', host='localhost', database='fuzzymamdani')
     cursor = conn.cursor()
     querySelect = """SELECT * FROM VARIABLES WHERE deleted = 0"""   
