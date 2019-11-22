e7791fc5129d0a446dd25b0fb998826b15c3462d
root <root@ip-172-31-34-68.ec2.internal>
2019-11-22 00:11:17 +0000

    tes

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
        //ENTER YOUR DATABASE CONNECTION INFO BELOW:
-         $hostname="localhost";
+         $hostname="ec2-34-207-250-194.compute-1.amazonaws.com";
          $database="fuzzymamdani";
          $username="root";
-         $password="";
+         $password="bevy2019";
 
    //DO NOT EDIT BELOW THIS LINE
      $link = mysqli_connect($hostname, $username, $password);
